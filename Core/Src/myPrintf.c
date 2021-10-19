09e92a74c556ab2cfef1803b1225c8665aafea6f
vgux <76217810+vgux@users.noreply.github.com>
2021-10-19 20:25:49 +0200

    commented myPrintf.c

--- Core/Src/myPrintf.c
+++ Core/Src/myPrintf.c
@@ -1,5 +1,10 @@
 #include <myPrintf.h>
 
+/**
+  * @brief  Implementation de la fonction printf pour le STM32
+  * @param  text: texte a afficher
+  * @retval None
+  */
 void myPrintf(uint8_t * text)
 {
 	HAL_UART_Transmit(&huart2, text, strlen(text), 1000);
