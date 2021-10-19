8aeb2eb1fd30334bd820fbab8f4a3d1e396b9685
vgux <76217810+vgux@users.noreply.github.com>
2021-10-19 20:22:04 +0200

    removed chaine from occurence printf

--- Core/Inc/occurence.h
+++ Core/Inc/occurence.h
@@ -17,5 +17,5 @@
 
 
 void occurence(uint8_t* chaine, uint32_t tab[256]);
-void occurencePrint(uint8_t* chaine, uint32_t tab[256]);
+void occurencePrint(uint32_t tab[256]);
 uint16_t obtenirNbCarDifferent(uint32_t tab[256]);
