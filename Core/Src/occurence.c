f63e3e7bdac1988b2477a6c39c31f18d903f712d
vgux <76217810+vgux@users.noreply.github.com>
2021-10-19 20:23:52 +0200

    commented occurence.c

--- Core/Src/occurence.c
+++ Core/Src/occurence.c
@@ -7,9 +7,13 @@
 
 
 #include "occurence.h"
-
+/**
+  * @brief  Obtention de l'occurence de chaque caractere
+  * @param  chaine: texte dans lequel on souhaite compter l'apparition de chaque caractère
+  * @param  tab: tableau contenant l'occurence de chaque caractere
+  * @retval None
+  */
 void occurence(uint8_t* chaine, uint32_t tab[256]) {
-  //Obtention des occurences de chaque caractere
   while(*chaine != '\0') {
 	  tab[*chaine]++;
 	  chaine++;
@@ -17,8 +21,12 @@ void occurence(uint8_t* chaine, uint32_t tab[256]) {
 
 }
 
-void occurencePrint(uint8_t* chaine, uint32_t tab[256]) {
-	  //Affiche chaque caractere ainsi que son occurence sous forme de tableau
+/**
+  * @brief  Affiche chaque caractere ainsi que son occurence sous forme de tableau
+  * @param  tab: Tableau des occurences
+  * @retval None
+  */
+void occurencePrint(uint32_t tab[256]) {
 	  printf("\n\n");
 	  printf("+-------+-------+\n");
 	  printf("| Car \t| Occ\t|\n");
@@ -30,6 +38,11 @@ void occurencePrint(uint8_t* chaine, uint32_t tab[256]) {
 
 }
 
+/**
+  * @brief  Calcul du nombre de caractere differents presents au sein du tableau des occurences
+  * @param  tab: Tableau des occurences
+  * @retval nbCarDifferent
+  */
 uint16_t obtenirNbCarDifferent(uint32_t tab[256]) {
 	uint16_t nbCarDifferent = 0;
 	for(uint8_t i = 0; i < 255; i++) {
